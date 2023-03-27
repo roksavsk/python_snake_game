b323c78754deb135f78420f223aa2ba7f8dafcb8
roksavsk <roxy.vsk@gmail.com>
2023-03-27 23:23:01 +0300

    Adding food

--- helpers.py
+++ helpers.py
@@ -1,9 +1,12 @@
+import random
+
+
 def message(dis, font_style, msg, color):
     msg = font_style.render(msg, True, color)
     dis.blit(msg, [30, 30])
 
 
-def has_intersects(w_width, w_height, x, y, snake_width, snake_height) -> bool:
+def has_intersects_borders(w_width, w_height, x, y, snake_width, snake_height) -> bool:
     if y <= 0:
         return True
     elif y >= w_height - snake_height:
@@ -13,3 +16,30 @@ def has_intersects(w_width, w_height, x, y, snake_width, snake_height) -> bool:
     elif x >= w_width - snake_width:
         return True
     return False
+
+
+def has_intersects_food(snake_x, snake_y,
+                        snake_x_append, snake_y_append,
+                        snake_width, snake_height,
+                        food_x, food_y,
+                        food_width, food_height) -> bool:
+    if food_x is None or food_y is None:
+        return False
+
+    if (snake_x + snake_width == food_x) and (snake_y == food_y) and snake_x_append:
+        return True  # left side
+    if (snake_x == food_x) and (snake_y + snake_height == food_y) and snake_y_append:
+        return True  # top side
+    if (snake_x == food_x + food_width) and (snake_y == food_y) and snake_x_append:
+        return True  # right side
+    if (snake_x == food_x) and (snake_y == food_y + food_height) and snake_y_append:
+        return True  # bottom side
+
+    return False
+
+
+def generate_food(w_width, w_height, snake_width, snake_height):
+    return (
+        round(random.randrange(0, w_width - snake_width) / 10.0) * 10.0,
+        round(random.randrange(0, w_height - snake_height) / 10.0) * 10.0
+    )
--- main.py
+++ main.py
@@ -1,28 +1,45 @@
+import random
 from time import sleep
 
 import pygame
-from helpers import message, has_intersects
+from helpers import message, has_intersects_borders, generate_food, has_intersects_food
 
 # CONSTANTS
-WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 300
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 500
 
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
+YELLOW = pygame.Color('yellow')
+FIREBRICK = pygame.Color('firebrick')
+darkslateblue = pygame.Color('darkslateblue')
+mediumseagreen = pygame.Color('mediumseagreen')
+springgreen3 = pygame.Color('springgreen3')
+royalblue4 = pygame.Color('royalblue4')
+royalblue = pygame.Color('royalblue')
+limegreen = pygame.Color('limegreen')
+orangered4 = pygame.Color('orangered4')
+indigo = pygame.Color('indigo')
+magenta3 = pygame.Color('magenta3')
 
-SNAKE_WIDTH = 10
-SNAKE_HEIGHT = 10
+SNAKE_WIDTH, SNAKE_HEIGHT = 10, 10
+FOOD_WIDTH, FOOD_HEIGHT = SNAKE_WIDTH, SNAKE_HEIGHT
 
-VELOCITY = 5
+
+VELOCITY = 10
 
 # VARIABLES
+SNAKE = []
 X = 200
 Y = 150
 
 X_APPEND = 0
 Y_APPEND = 0
+
+FOOD_X = 0
+FOOD_Y = 0
 
 # Start
 pygame.init()
@@ -31,20 +48,47 @@ pygame.display.update()
 pygame.display.set_caption('Snake game!')
 
 clock = pygame.time.Clock()
-font_style = pygame.font.SysFont(None, 50)
-
-# Start screen
-# dis.fill(BLUE)
-# pygame.display.flip()
-# sleep(1)
-# dis.fill(GREEN)
-# pygame.display.flip()
-# sleep(1)
-
+font_style = pygame.font.SysFont('Tahoma', 30)
 game_run = True
+game_end = False
+game_before_init = True
+
+# Start screen animation
+dis.fill(royalblue)
+pygame.display.flip()
+sleep(0.6)
+dis.fill(limegreen)
+pygame.display.flip()
+sleep(0.6)
+dis.fill(YELLOW)
+pygame.display.flip()
+sleep(0.6)
+
 while game_run:
+
+    while game_end:
+        message(dis, font_style, "Game Over. Press C to continue or Q to exit", indigo)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    game_end = False
+                    game_run = True
+                    X = 200
+                    Y = 150
+                    X_APPEND = 0
+                    Y_APPEND = 0
+
+                if event.key == pygame.K_q:
+                    game_end = False
+                    game_run = False
+                    break
+
     for event in pygame.event.get():
         print(event)
+
         if event.type == pygame.QUIT:
             game_run = False
 
@@ -52,30 +96,55 @@ while game_run:
             if event.key == pygame.K_LEFT:
                 X_APPEND = -VELOCITY
                 Y_APPEND = 0
+                if game_before_init:
+                    FOOD_X, FOOD_Y = generate_food(WINDOW_WIDTH, WINDOW_HEIGHT, SNAKE_WIDTH, SNAKE_HEIGHT)
+                    game_before_init = False
+
             elif event.key == pygame.K_RIGHT:
                 X_APPEND = VELOCITY
                 Y_APPEND = 0
+                if game_before_init:
+                    FOOD_X, FOOD_Y = generate_food(WINDOW_WIDTH, WINDOW_HEIGHT, SNAKE_WIDTH, SNAKE_HEIGHT)
+                    game_before_init = False
+
             elif event.key == pygame.K_UP:
                 X_APPEND = 0
                 Y_APPEND = -VELOCITY
+                if game_before_init:
+                    FOOD_X, FOOD_Y = generate_food(WINDOW_WIDTH, WINDOW_HEIGHT, SNAKE_WIDTH, SNAKE_HEIGHT)
+                    game_before_init = False
+
             elif event.key == pygame.K_DOWN:
                 X_APPEND = 0
                 Y_APPEND = VELOCITY
+                if game_before_init:
+                    FOOD_X, FOOD_Y = generate_food(WINDOW_WIDTH, WINDOW_HEIGHT, SNAKE_WIDTH, SNAKE_HEIGHT)
+                    game_before_init = False
 
-    if has_intersects(WINDOW_WIDTH, WINDOW_HEIGHT, X, Y, SNAKE_WIDTH, SNAKE_HEIGHT):
-        game_run = False
+    if has_intersects_borders(WINDOW_WIDTH, WINDOW_HEIGHT, X, Y, SNAKE_WIDTH, SNAKE_HEIGHT):
+        game_end = True
+
+    if has_intersects_food(X, Y, X_APPEND, Y_APPEND, SNAKE_WIDTH, SNAKE_HEIGHT, FOOD_X, FOOD_Y, FOOD_WIDTH, FOOD_HEIGHT):
+        FOOD_X, FOOD_Y = generate_food(WINDOW_WIDTH, WINDOW_HEIGHT, SNAKE_WIDTH, SNAKE_HEIGHT)
 
     X += X_APPEND
     Y += Y_APPEND
 
-    dis.fill(WHITE)
-    pygame.draw.rect(dis, BLUE, [X, Y, SNAKE_WIDTH, SNAKE_HEIGHT])
+    snake_head = [X, Y]
+    SNAKE.append(snake_head)
+
+    if len(SNAKE) > 1:
+        del SNAKE[0]
+
+    # Draw
+    dis.fill(springgreen3)
+    pygame.draw.rect(dis, royalblue4, [X, Y, SNAKE_WIDTH, SNAKE_HEIGHT])
+
+    if FOOD_X is not None and FOOD_Y is not None:
+        pygame.draw.rect(dis, magenta3, [FOOD_X, FOOD_Y, FOOD_WIDTH, FOOD_HEIGHT])
+
     pygame.display.update()
     clock.tick(10)
-
-message(dis, font_style, "Game Over", RED)
-pygame.display.update()
-sleep(1)
 
 pygame.quit()
 quit()
