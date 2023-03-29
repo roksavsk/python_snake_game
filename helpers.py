ad955e772a49bc30eaf0b2e5eb902202b7eef268
roksavsk <roxy.vsk@gmail.com>
2023-03-29 12:38:06 +0300

    Adding obstacles

--- helpers.py
+++ helpers.py
@@ -1,9 +1,9 @@
 import random
 
 
-def message(dis, font_style, msg, color):
+def message(dis, font_style, msg, color, x, y):
     msg = font_style.render(msg, True, color)
-    dis.blit(msg, [30, 30])
+    dis.blit(msg, [x, y])
 
 
 def has_intersects_borders(w_width, w_height, x, y, snake_width, snake_height) -> bool:
@@ -38,8 +38,78 @@ def has_intersects_food(snake_x, snake_y,
     return False
 
 
+def snake_intersects_block(snake_x, snake_y,
+                           snake_width, snake_height,
+                           direction_x,
+                           block_x, block_y,
+                           block_width, block_height) -> bool:
+    """
+
+    :param snake_x:
+    :param snake_y:
+    :param snake_width:
+    :param snake_height:
+    :param direction_x: True if direction on Y axis, False - Y
+    :param block_x:
+    :param block_y:
+    :param block_width:
+    :param block_height:
+    :return:
+    """
+
+    # if check_direction:
+    #     direction_y = not direction_x
+    #     direction_x = direction_x
+    # else:
+    #     direction_y = True
+    #     direction_x = True
+    direction_y = not direction_x
+
+    if block_x is None or block_y is None:
+        return False
+
+    if (snake_x + snake_width == block_x) and (snake_y == block_y) and direction_x:
+        return True  # left side
+    if (snake_x == block_x) and (snake_y + snake_height == block_y) and direction_y:
+        return True  # top side
+    if (snake_x == block_x + block_width) and (snake_y == block_y) and direction_x:
+        return True  # right side
+    if (snake_x == block_x) and (snake_y == block_y + block_height) and direction_y:
+        return True  # bottom side
+
+    return False
+
+
+def snake_intersects_itself(snake):
+    for block in snake[:-1]:
+        if block == snake[-1]:
+            return True
+
+
+def snake_intersects_brick(snake_head, bricks):
+    for block in bricks:
+        if block == snake_head:
+            return True
+
+
+def to_ten(n):
+    return round(n / 10.0) * 10.0
+
+
 def generate_food(w_width, w_height, snake_width, snake_height):
     return (
-        round(random.randrange(0, w_width - snake_width) / 10.0) * 10.0,
-        round(random.randrange(0, w_height - snake_height) / 10.0) * 10.0
+        to_ten(random.randrange(0, w_width - snake_width)),
+        to_ten(random.randrange(0, w_height - snake_height))
     )
+
+
+def generate_wall(w_width, w_height, brick_size) -> list:
+    bricks_x = int(to_ten(w_width / 2))
+    brick_y_start = int(to_ten(w_height / 4))
+    brick_y_end = int(to_ten(w_height - w_height / 4))
+    return [[bricks_x, y] for y in range(brick_y_start, brick_y_end, brick_size)]
+
+
+def generate_bricks(w_width, w_height, snake_width, snake_height):
+    return [[to_ten(random.randrange(0, w_width - snake_width)), to_ten(random.randrange(0, w_height - snake_height))]
+            for x in range(10)]
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import random
 from time import sleep
 
 import pygame
-from helpers import message, has_intersects_borders, generate_food, has_intersects_food
+from helpers import message, has_intersects_borders, generate_food, snake_intersects_block, \
+    snake_intersects_itself, generate_wall, snake_intersects_brick, generate_bricks
 
 # CONSTANTS
 WINDOW_WIDTH = 800
@@ -12,6 +13,7 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 YELLOW = pygame.Color('yellow')
 FIREBRICK = pygame.Color('firebrick')
 darkslateblue = pygame.Color('darkslateblue')
@@ -23,15 +25,19 @@ limegreen = pygame.Color('limegreen')
 orangered4 = pygame.Color('orangered4')
 indigo = pygame.Color('indigo')
 magenta3 = pygame.Color('magenta3')
+slategrey = pygame.Color('slategrey')
 
 SNAKE_WIDTH, SNAKE_HEIGHT = 10, 10
 FOOD_WIDTH, FOOD_HEIGHT = SNAKE_WIDTH, SNAKE_HEIGHT
-
+BRICK_SIZE = 10
 
 VELOCITY = 10
 
 # VARIABLES
 SNAKE = []
+WALL = generate_wall(WINDOW_WIDTH, WINDOW_HEIGHT, BRICK_SIZE)
+BRICKS = generate_bricks(WINDOW_WIDTH, WINDOW_HEIGHT, SNAKE_WIDTH, SNAKE_HEIGHT)
+
 X = 200
 Y = 150
 
@@ -41,6 +47,13 @@ Y_APPEND = 0
 FOOD_X = 0
 FOOD_Y = 0
 
+SCORE = 0
+
+
+def direction_x_by_append(x_append):
+    return True if x_append else False
+
+
 # Start
 pygame.init()
 dis = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -48,30 +61,40 @@ pygame.display.update()
 pygame.display.set_caption('Snake game!')
 
 clock = pygame.time.Clock()
-font_style = pygame.font.SysFont('Tahoma', 30)
+font_style_start = pygame.font.SysFont('Arial Black', 50)
+font_style = pygame.font.SysFont('Arial Black', 30)
+font_style_score = pygame.font.SysFont('Tahoma', 25)
 game_run = True
 game_end = False
 game_before_init = True
+new_head = False
 
 # Start screen animation
 dis.fill(royalblue)
 pygame.display.flip()
-sleep(0.6)
+sleep(0.4)
 dis.fill(limegreen)
 pygame.display.flip()
-sleep(0.6)
+sleep(0.4)
 dis.fill(YELLOW)
+message(dis, font_style_start, "Let's go!", royalblue4, 275, 200)
+pygame.display.update()
 pygame.display.flip()
-sleep(0.6)
+sleep(1)
 
 while game_run:
+    dis.fill(springgreen3)
+    message(dis, font_style_score, f"Score: {SCORE}", indigo, 30, 30)
 
     while game_end:
-        message(dis, font_style, "Game Over. Press C to continue or Q to exit", indigo)
+        message(dis, font_style, "Game Over. Press C to continue or Q to exit", YELLOW, 30, 200)
         pygame.display.update()
 
         for event in pygame.event.get():
-            print(event)
+            if event.type == pygame.QUIT:
+                game_run = False
+                game_end = False
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_c:
                     game_end = False
@@ -80,14 +103,15 @@ while game_run:
                     Y = 150
                     X_APPEND = 0
                     Y_APPEND = 0
+                    SNAKE = [[X, Y]]
+                    SCORE = 0
 
                 if event.key == pygame.K_q:
                     game_end = False
                     game_run = False
-                    break
 
     for event in pygame.event.get():
-        print(event)
+        # print(event)
 
         if event.type == pygame.QUIT:
             game_run = False
@@ -124,8 +148,16 @@ while game_run:
     if has_intersects_borders(WINDOW_WIDTH, WINDOW_HEIGHT, X, Y, SNAKE_WIDTH, SNAKE_HEIGHT):
         game_end = True
 
-    if has_intersects_food(X, Y, X_APPEND, Y_APPEND, SNAKE_WIDTH, SNAKE_HEIGHT, FOOD_X, FOOD_Y, FOOD_WIDTH, FOOD_HEIGHT):
+    if snake_intersects_block(
+            X, Y,
+            SNAKE_WIDTH, SNAKE_HEIGHT,
+            direction_x_by_append(X_APPEND),
+            FOOD_X, FOOD_Y,
+            FOOD_WIDTH, FOOD_HEIGHT
+    ):
         FOOD_X, FOOD_Y = generate_food(WINDOW_WIDTH, WINDOW_HEIGHT, SNAKE_WIDTH, SNAKE_HEIGHT)
+        new_head = True
+        SCORE += 1
 
     X += X_APPEND
     Y += Y_APPEND
@@ -136,12 +168,33 @@ while game_run:
     if len(SNAKE) > 1:
         del SNAKE[0]
 
-    # Draw
-    dis.fill(springgreen3)
-    pygame.draw.rect(dis, royalblue4, [X, Y, SNAKE_WIDTH, SNAKE_HEIGHT])
+    if new_head:
+        X += X_APPEND
+        Y += Y_APPEND
+        SNAKE.append([X, Y])
+        new_head = False
 
+    if snake_intersects_itself(SNAKE):
+        game_end = True
+
+    if snake_intersects_brick(SNAKE[-1], WALL):
+        game_end = True
+
+    if snake_intersects_brick(SNAKE[-1], BRICKS):
+        game_end = True
+
+    # Draw
     if FOOD_X is not None and FOOD_Y is not None:
         pygame.draw.rect(dis, magenta3, [FOOD_X, FOOD_Y, FOOD_WIDTH, FOOD_HEIGHT])
+
+    for coord_block in SNAKE:
+        pygame.draw.rect(dis, royalblue4, [coord_block[0], coord_block[1], SNAKE_WIDTH, SNAKE_HEIGHT])
+
+    for brick in WALL:
+        pygame.draw.rect(dis, slategrey, [brick[0], brick[1], BRICK_SIZE, BRICK_SIZE])
+
+    for brick in BRICKS:
+        pygame.draw.rect(dis, slategrey, [brick[0], brick[1], BRICK_SIZE, BRICK_SIZE])
 
     pygame.display.update()
     clock.tick(10)
